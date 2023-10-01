bf04ba7023261875025781803699eddca56d86d1
Ketan Dabre <ketandabre99per@gmail.com>
2023-10-01 19:55:06 +0530

    added socket fuctionality

--- app.js
+++ app.js
@@ -4,4 +4,26 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
+const  io = require('socket.io' )(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+let socketsConnected = new Set()
+
+// io.on('connection', (socket) => {
+//       console.log(socket.id)
+// })
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+      console.log(socket.id) 
+      socketsConnected.add(socket.id)
+
+      io.emit('clients-total', socketsConnected.size)
+
+      socket.on('disconnect', () => {
+            console.log('Socket disconnected', socket.id)
+            socketsConnected.delete(socket.id)
+            io.emit("clients-total", socketsConnected.size);
+      })
+}
